1be748ae2f62b53601d3e2887b29b9e8c016aaae
ADAM-PROG2025 <at2570456@gmail.com>
2025-07-26 10:46:28 +0000

    This the main file for the project

---
+++ division.py
@@ -0,0 +1,8 @@
+
+dividende = int(input("Enter the dividende: "))
+divisor = int(input("Enter the divisor: "))
+
+quotient = dividende//divisor
+rest = dividende % divisor
+
+print(f"the division of {dividende} and {divisor} give :\n quotient: {quotient}\n rest: {rest}")
